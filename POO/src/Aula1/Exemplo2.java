b7da9215a11c00b43acf08debba1ad1ead839e02
Fabio Gomes Rocha <prof.fabiogomesrocha@gmail.com>
2017-07-30 22:06:08 -0300

    Atualização Aula POO - Aula 1 (Exemplos de variáveis e de entrada de dados)

--- POO/src/Aula1/Exemplo2.java
+++ POO/src/Aula1/Exemplo2.java
@@ -6,6 +6,7 @@
 package Aula1;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,9 +17,16 @@ public class Exemplo2 {
 		//Exemplo de entrada de dados
 		Scanner s = new Scanner(System.in);
 		System.out.print("Entre com seu nome: ");
+		//le até que o usuário pressionar enter
 		String nome = s.nextLine();
 		System.out.println("Seu nome é " + nome);
-			
+		System.out.println("Qual a sua idade?");
+		//Lê numero inteiro
+		int num = s.nextInt();
+		System.out.println("Sua idade é: " + num);
+		//Entrada de dados pelo modo gráfico
+		nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+		JOptionPane.showMessageDialog(null, "Seu nome é " + nome);
 		
 	}
 	
