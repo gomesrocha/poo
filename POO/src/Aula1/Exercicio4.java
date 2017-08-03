9c3110a99ff2b3d8511fd75479630c5ee41d2a1f
10361 <10361@BRG504K73W.acd.grupotiradentes.com>
2017-08-03 20:10:55 -0300

    Atualização dos exercícios da primeira aula

---
+++ POO/src/Aula1/Exercicio4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula1;
+
+import java.util.Calendar;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 10361
+ */
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        int dia, mes, ano;
+        mes = cal.get(Calendar.MONTH);
+        dia = cal.get(Calendar.DAY_OF_MONTH);
+        ano = cal.get(Calendar.YEAR);
+        JOptionPane.showMessageDialog(null, "Hoje é dia " + dia + " do mês "
+                + mes +  " de " + ano);
+    }
+    
+}
