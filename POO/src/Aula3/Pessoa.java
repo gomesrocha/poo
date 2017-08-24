888a63e596e5611a363a9f3bb1373a3d4be44849
10361 <10361@BRG504K73W.acd.grupotiradentes.com>
2017-08-24 19:27:46 -0300

    Atualização POO com refatoração da classe pessoa, inserindo associação a endereço

--- POO/src/Aula3/Pessoa.java
+++ POO/src/Aula3/Pessoa.java
@@ -17,26 +17,18 @@ public class Pessoa {
    protected String dtNasc;
    protected String cpf;
    protected String rg;
-   protected String logradouro;
-   protected String numero;
-   protected String complemento;
-   protected String cep;
-   protected String bairro;
-   protected String cidade;
-   protected String uf;
+   protected Endereco endereco;
    protected String email;
    protected String telefone;
 
+  public Pessoa() {
+      endereco = new Endereco();
+   }
+
+   
    public void cadastrarPessoa() {
       nome = JOptionPane.showInputDialog("Entre com seu nome");
       dtNasc = JOptionPane.showInputDialog("Entre com a data de nascimento");
-      logradouro = JOptionPane.showInputDialog("Entre com o Endereço");
-      numero = JOptionPane.showInputDialog("Entre com o numero");
-      complemento = JOptionPane.showInputDialog("Entre com o Complemento");
-      cep = JOptionPane.showInputDialog("Entre com o CEP");
-      bairro = JOptionPane.showInputDialog("Entre com o bairro");
-      cidade = JOptionPane.showInputDialog("Entre com a cidade");
-      uf = JOptionPane.showInputDialog("Entre com o UF");
       cpf = JOptionPane.showInputDialog("Entre com o CPF");
       while (cpf.length() != 11) {
          JOptionPane.showMessageDialog(null, "CPF inválido");
@@ -45,22 +37,17 @@ public class Pessoa {
       rg = JOptionPane.showInputDialog("Entre com o RG");
       email = JOptionPane.showInputDialog("Entre com o email");
       telefone = JOptionPane.showInputDialog("Entre com o celular");
+      endereco.cadastrarEndereco();
    }
 
    public String listarPessoa() {
       String dados = "Nome: " + nome
               + "\nData de Nascimento: " + dtNasc
-              + "\nEndereço: " + logradouro
-              + "\nNumero: " + numero
-              + "\nComplemento: " + complemento
-              + "\nCEP: " + cep
-              + "\nBairro: " + bairro
-              + "\nCidade: " + cidade
-              + "\nEstado: " + uf
               + "\nCPF: " + cpf
               + "\nRG: " + rg
               + "\nE-Mail: " + email
-              + "\nTelefone: " + telefone;
+              + "\nTelefone: " + telefone
+              + endereco.listarEndereco();
       return dados;
    }
 
