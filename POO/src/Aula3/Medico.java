f8cd92748aaa3a86a3a421c31a8e69bb73dad8b7
10361 <10361@BRG504K73W.acd.grupotiradentes.com>
2017-08-24 21:56:26 -0300

    Atualização POO com refatoração da classe pessoa, inserindo associação a endereço

--- POO/src/Aula3/Medico.java
+++ POO/src/Aula3/Medico.java
@@ -15,6 +15,11 @@ public class Medico extends Pessoa{
    private String crm;
    private Telefone celular;
    private Endereco endProf;
+   private double salarioBruto;
+   private double inss;
+   private double irrf;
+   private double salarioSemInss;
+   private double salarioLiquido;
 
    public Medico() {
       celular = new Telefone();
@@ -35,5 +40,34 @@ public class Medico extends Pessoa{
               + endProf.listarEndereco());
       
    }
+     public void calcularSalario(int tipo) {
+        if(tipo==1){        
+            salarioBruto = 3225.33;
+        } else if (tipo==2){
+            salarioBruto = 6600.20;
+        }
+        if (salarioBruto <= 1556.94) {
+            inss = salarioBruto / 100 * 8;
+        } else if (salarioBruto > 1556.94 && salarioBruto <= 2594.92) {
+            inss = salarioBruto / 100 * 9;
+        } else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82) {
+            inss = salarioBruto / 100 * 11;
+        } else if (salarioBruto > 5189.82) {
+            inss = 5189.82 / 100 * 11;
+        }
+        salarioSemInss = salarioBruto - inss;
+        if(salarioSemInss <= 1903.98){
+            irrf=0;
+        } else if(salarioSemInss >=1903.99 && salarioSemInss <= 2826.65){
+            irrf = (salarioSemInss - 1903.99)/100 * 7.5;
+        } else if(salarioSemInss > 2826.65 && salarioSemInss <= 3751.05){
+            irrf = (salarioSemInss - 1903.99)/100 * 15;
+        } else if(salarioSemInss > 3751.05 && salarioSemInss <= 4664.68){
+            irrf = (salarioSemInss - 1903.99)/100 * 22.5;
+        } else if(salarioSemInss > 4664.68){
+            irrf = (salarioSemInss - 1903.99)/100 * 27.5;
+        }
+        salarioLiquido = salarioSemInss - irrf;
+    }
    
 }
